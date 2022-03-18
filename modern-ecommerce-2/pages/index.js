66fbfb89d8571e6f7aff9098d8aaa4db6e496ee4
Shayne <shayne.fawcett@gmail.com>
2022-03-18 12:17:42 -0400

    first shopify call for all products in collection handle

--- modern-ecommerce-2/pages/index.js
+++ modern-ecommerce-2/pages/index.js
@@ -1,7 +1,16 @@
+import { getProductsInCollection } from "../lib/shopify"
 
-
-export default function Home() {
+export default function Home({products}) {
+  console.log(products)
   return (
     <div className="text-3xl">Hello from Shopify Next js</div>
   )
+}
+
+export async function getStaticProps() {
+  const products = await getProductsInCollection()
+
+  return {
+    props: { products }, // will be passed to the page component as props
+  }
 }
