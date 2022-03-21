5a1f33d4f5e46ea990ceb4df1160e29541108c80
Shayne <shayne.fawcett@gmail.com>
2022-03-21 17:49:09 -0400

    hero section on homepage

--- modern-ecommerce-2/pages/index.js
+++ modern-ecommerce-2/pages/index.js
@@ -1,12 +1,16 @@
 import { getProductsInCollection } from "../lib/shopify"
 import ProductList from '../components/ProductList'
+import Hero from "../components/Hero"
 
 export default function Home({products}) {
   console.log(products)
   return (
-    <div className="text-3xl">
-      <ProductList products={products} />
-    </div>
+    <>
+      <Hero />
+      <div className="text-3xl">
+        <ProductList products={products} />
+      </div>
+    </>
   )
 }
 
