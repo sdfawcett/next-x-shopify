143e4e83da3b96d02dc4a6c31b1b81ab4d0abf7d
Shayne <shayne.fawcett@gmail.com>
2022-04-05 12:08:40 -0400

    hero fix

--- modern-ecommerce-2/components/Hero.js
+++ modern-ecommerce-2/components/Hero.js
@@ -5,29 +5,34 @@ import WomanHeroBg from '../images/woman-hero-bg.jpg';
 
 export default function Hero() {
   return (
-    <section>
-
-      <div className="hero min-h-screen">
-        
+    <section id='hero' className="relative bg-white">
       <Image
             src={WomanHeroBg}
             alt="Woman holding CBD tincture bottle"
-            className='object-center'
+            className='absolute inset-0 object-[75%] sm:object-[25%] object-cover w-full h-full opacity-25 sm:opacity-100'
             layout="fill" 
             objectFit="cover" 
             placeholder="blur"
             objectPosition="center"
 
         />
-        <div className="hero-content z-10 text-center dark:border dark:border-gray-800 mx-3.5">
-          <div className="max-w-md">
+
+
+        <div className="hidden sm:block sm:inset-0 sm:absolute sm:bg-gradient-to-r sm:from-white sm:to-transparent"></div>
+
+        <div className="relative max-w-screen-xl px-4 py-32 mx-auto lg:h-screen lg:items-center lg:flex">
+          <div className="max-w-xl text-center sm:text-left">
+
             <h1 className="mb-5 text-5xl h2 font-bold font-lora dark:text-gray-800">Premium CBD Products You Can Trust</h1>
             <p className="mb-5 dark:text-gray-700 font-karla">Made from the highest quality, lab tested, Oregon hemp plants</p>
-            <button className="btn btn-primary">Shop Now</button>
+
+            <div className="flex flex-wrap gap-4 mt-8 text-center">
+              <a className="block w-full px-12 py-3 text-sm font-medium text-white rounded shadow bg-lighter-green sm:w-auto active:bg-bg-green hover:bg-bg-green focus:outline-none focus:ring" href="/get-started">
+                Get Started
+              </a>
+            </div>
           </div>
         </div>
-      </div>
-
-    </section>
+      </section>
   )
 }
