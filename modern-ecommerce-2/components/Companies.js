86650940e91ba1036c0f116d727669f7ae7c7e48
Shayne <shayne.fawcett@gmail.com>
2022-04-12 18:28:41 -0400

    image optimizations

--- modern-ecommerce-2/components/Companies.js
+++ modern-ecommerce-2/components/Companies.js
@@ -18,104 +18,138 @@ export default function Companies() {
             <Marquee gradient={false} speed={40} loop={0}>
                <span className="mx-28 text-xl text-center">
                   "World's finest CBD brand for relaxation."<br />
-                  <Image src={RollingStoneLogo} height={40} width={100} />
+                  <Image src={RollingStoneLogo} height={40} width={100} alt='rolling stone logo' />
                </span>
                <span className="mx-28 text-xl text-center">
                   "The hottest CBD brand around right now."<br />
-                  <Image src={MaximLogo} height={40} width={100} />
+                  <Image src={MaximLogo} height={40} width={100} alt='maxim logo' />
                </span>
                <span className="mx-28 text-xl text-center">
                   "Thanks to Bomb Botanicals, we're living our best lives."<br />
-                  <Image src={TimeLogo} height={40} width={100} />
+                  <Image src={TimeLogo} height={40} width={100} alt='time logo' />
                </span>
                <span className="mx-28 text-xl text-center">
                   "One of the best THC-free CBD oils that we've ever tried."<br />
-                  <Image src={VarietyLogo} height={40} width={100} />
+                  <Image src={VarietyLogo} height={40} width={100} alt='variety logo' />
                </span>
 
                <span className="mx-28 text-xl text-center">
                   "The CBD brand at the top of our list for a number of reasons."<br />
-                  <Image src={ViceLogo} height={40} width={100} />
+                  <Image src={ViceLogo} height={40} width={100} alt='vice logo' />
                </span>
                <span className="mx-28 text-xl text-center">
                   "FAB CBD continues to push the envelope when it comes to product design and development."<br />
-                  <Image src={NewYorkerLogo} height={40} width={100} />
+                  <Image src={NewYorkerLogo} height={40} width={100} alt='new yorker logo' />
                </span>
                <span className="mx-28 text-xl text-center">
                   "In comparison to other CBD brands, FAB CBD’s prices are comparable or more affordable."<br />
-                  <Image src={EntertainmentWeeklyLogo} height={40} width={100} />
+                  <Image src={EntertainmentWeeklyLogo} height={40} width={100} alt='entertainment weekly logo' />
                </span>
                <span className="mx-28 text-xl text-center">
                   "Affordable prices for strong doses."<br />
-                  <Image src={NewYorkTimesLogo} height={40} width={100} />
+                  <Image src={NewYorkTimesLogo} height={40} width={100} alt='new york times logo' />
                </span>
 
                <span className="mx-28 text-xl text-center">
                   "World's finest CBD brand for relaxation."<br />
-                  <Image src={RollingStoneLogo} height={40} width={100} />
+                  <Image src={RollingStoneLogo} height={40} width={100} alt='rolling stone logo' />
                </span>
                <span className="mx-28 text-xl text-center">
                   "The hottest CBD brand around right now."<br />
-                  <Image src={MaximLogo} height={40} width={100} />
+                  <Image src={MaximLogo} height={40} width={100} alt='maxim logo' />
                </span>
                <span className="mx-28 text-xl text-center">
                   "Thanks to Bomb Botanicals, we're living our best lives."<br />
-                  <Image src={TimeLogo} height={40} width={100} />
+                  <Image src={TimeLogo} height={40} width={100} alt='time logo' />
                </span>
                <span className="mx-28 text-xl text-center">
                   "One of the best THC-free CBD oils that we've ever tried."<br />
-                  <Image src={VarietyLogo} height={40} width={100} />
+                  <Image src={VarietyLogo} height={40} width={100} alt='variety logo' />
                </span>
 
                <span className="mx-28 text-xl text-center">
                   "The CBD brand at the top of our list for a number of reasons."<br />
-                  <Image src={ViceLogo} height={40} width={100} />
+                  <Image src={ViceLogo} height={40} width={100} alt='vice logo' />
                </span>
                <span className="mx-28 text-xl text-center">
                   "FAB CBD continues to push the envelope when it comes to product design and development."<br />
-                  <Image src={NewYorkerLogo} height={40} width={100} />
+                  <Image src={NewYorkerLogo} height={40} width={100} alt='new yorker logo' />
                </span>
                <span className="mx-28 text-xl text-center">
                   "In comparison to other CBD brands, FAB CBD’s prices are comparable or more affordable."<br />
-                  <Image src={EntertainmentWeeklyLogo} height={40} width={100} />
+                  <Image src={EntertainmentWeeklyLogo} height={40} width={100} alt='entertainment weekly logo' />
                </span>
                <span className="mx-28 text-xl text-center">
                   "Affordable prices for strong doses."<br />
-                  <Image src={NewYorkTimesLogo} height={40} width={100} />
+                  <Image src={NewYorkTimesLogo} height={40} width={100} alt='new york times logo' />
                </span>
 
                <span className="mx-28 text-xl text-center">
                   "World's finest CBD brand for relaxation."<br />
-                  <Image src={RollingStoneLogo} height={40} width={100} />
+                  <Image src={RollingStoneLogo} height={40} width={100} alt='rolling stone logo' />
                </span>
                <span className="mx-28 text-xl text-center">
                   "The hottest CBD brand around right now."<br />
-                  <Image src={MaximLogo} height={40} width={100} />
+                  <Image src={MaximLogo} height={40} width={100} alt='maxim logo' />
                </span>
                <span className="mx-28 text-xl text-center">
                   "Thanks to Bomb Botanicals, we're living our best lives."<br />
-                  <Image src={TimeLogo} height={40} width={100} />
+                  <Image src={TimeLogo} height={40} width={100} alt='time logo' />
                </span>
                <span className="mx-28 text-xl text-center">
                   "One of the best THC-free CBD oils that we've ever tried."<br />
-                  <Image src={VarietyLogo} height={40} width={100} />
+                  <Image src={VarietyLogo} height={40} width={100} alt='variety logo' />
                </span>
 
                <span className="mx-28 text-xl text-center">
                   "The CBD brand at the top of our list for a number of reasons."<br />
-                  <Image src={ViceLogo} height={40} width={100} />
+                  <Image src={ViceLogo} height={40} width={100} alt='vice logo' />
                </span>
                <span className="mx-28 text-xl text-center">
                   "FAB CBD continues to push the envelope when it comes to product design and development."<br />
-                  <Image src={NewYorkerLogo} height={40} width={100} />
+                  <Image src={NewYorkerLogo} height={40} width={100} alt='new yorker logo' />
                </span>
                <span className="mx-28 text-xl text-center">
                   "In comparison to other CBD brands, FAB CBD’s prices are comparable or more affordable."<br />
-                  <Image src={EntertainmentWeeklyLogo} height={40} width={100} />
+                  <Image src={EntertainmentWeeklyLogo} height={40} width={100} alt='entertainment weekly logo' />
                </span>
                <span className="mx-28 text-xl text-center">
                   "Affordable prices for strong doses."<br />
-                  <Image src={NewYorkTimesLogo} height={40} width={100} />
+                  <Image src={NewYorkTimesLogo} height={40} width={100} alt='new york times logo' />
+               </span>
+
+               <span className="mx-28 text-xl text-center">
+                  "World's finest CBD brand for relaxation."<br />
+                  <Image src={RollingStoneLogo} height={40} width={100} alt='rolling stone logo' />
+               </span>
+               <span className="mx-28 text-xl text-center">
+                  "The hottest CBD brand around right now."<br />
+                  <Image src={MaximLogo} height={40} width={100} alt='maxim logo' />
+               </span>
+               <span className="mx-28 text-xl text-center">
+                  "Thanks to Bomb Botanicals, we're living our best lives."<br />
+                  <Image src={TimeLogo} height={40} width={100} alt='time logo' />
+               </span>
+               <span className="mx-28 text-xl text-center">
+                  "One of the best THC-free CBD oils that we've ever tried."<br />
+                  <Image src={VarietyLogo} height={40} width={100} alt='variety logo' />
+               </span>
+
+               <span className="mx-28 text-xl text-center">
+                  "The CBD brand at the top of our list for a number of reasons."<br />
+                  <Image src={ViceLogo} height={40} width={100} alt='vice logo' />
+               </span>
+               <span className="mx-28 text-xl text-center">
+                  "FAB CBD continues to push the envelope when it comes to product design and development."<br />
+                  <Image src={NewYorkerLogo} height={40} width={100} alt='new yorker logo' />
+               </span>
+               <span className="mx-28 text-xl text-center">
+                  "In comparison to other CBD brands, FAB CBD’s prices are comparable or more affordable."<br />
+                  <Image src={EntertainmentWeeklyLogo} height={40} width={100} alt='entertainment weekly logo' />
+               </span>
+               <span className="mx-28 text-xl text-center">
+                  "Affordable prices for strong doses."<br />
+                  <Image src={NewYorkTimesLogo} height={40} width={100} alt='new york times logo' />
                </span>
                
             </Marquee>
